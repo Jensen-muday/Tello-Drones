971d6aa82a04508f7986440d9812ea1531d0b0e0
MudayJ <02jensen.muday@gulllakecs.org>
2024-02-13 08:29:06 -0500

    2.13.2024
    circle commit

--- SandBoxPlayGround.py
+++ SandBoxPlayGround.py
@@ -4,14 +4,11 @@
 
 import threading, socket, sys, time, subprocess
 
-
 # GLOBAL VARIABLES DECLARED HERE....
 host = ''
 port = 9000
-locaddr = (host,port)
-tello_address = ('192.168.10.1', 8889) # Get the Tello drone's address
-
-
+locaddr = (host, port)
+tello_address = ('192.168.10.1', 8889)  # Get the Tello drone's address
 
 # Creates a UDP socketd
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,20 +23,20 @@ def recv():
             data, server = sock.recvfrom(1518)
             print(data.decode(encoding="utf-8"))
         except Exception:
-            print ('\n****Keep Eye on Drone****\n')
+            print('\n****Keep Eye on Drone****\n')
             break
 
 
-def sendmsg(msg, sleep = 6):
+def sendmsg(msg, sleep=6):
     print("Sending: " + msg)
     msg = msg.encode(encoding="utf-8")
     sock.sendto(msg, tello_address)
     time.sleep(sleep)
 
+
 # recvThread create
 recvThread = threading.Thread(target=recv)
 recvThread.start()
-
 
 # CREATE FUNCTIONS HERE....
 
@@ -52,16 +49,14 @@ print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
 print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
 ready = input('\nAre you ready to take flight: ')
 
-
 try:
     if ready.lower() == 'yes':
         print("\nStarting Drone!\n")
 
         sendmsg('command', 0)
-        sendmsg('battery', 4)
         sendmsg('takeoff')
         '''
-    # Square - Pilot = Avonlea Thalmann - CoPilot = Jensen Muday
+     # Square - Pilot = Avonlea Thalmann - CoPilot = Jensen Muday
         sendmsg('forward 100', )
         sendmsg('cw 90')
         sendmsg('forward 100')
@@ -70,14 +65,15 @@ try:
         sendmsg('cw 90')
         sendmsg('forward 100')
         sendmsg('cw 90',)
-        '''
 
-    # Triangle - Pilot = Jensen Muday - CoPilot = Avonlea Thalmann
+
+     # Triangle - Pilot = Jensen Muday - CoPilot = Avonlea Thalmann
         for i in range (3):
             sendmsg('forward 80', 10)
             sendmsg('cw 120', 10)
-
-    # Circle - Pilot = Avonlea Thalmann - CoPilot = Jensen Muday
+    '''
+        # Circle - Pilot = Avonlea Thalmann - CoPilot = Jensen Muday
+        sendmsg('curve 25, -25, 0, 25, -75, 0, 30')
 
         sendmsg('land')
 
